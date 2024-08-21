1c6c8cb32419e048efc3efcdae3229e9f04ab48f
Robert Kirkpatrick <robert.kirkp14@gmail.com>
2024-08-21 11:29:32 -0500

    add firebase functions project

---
+++ apps/server-app-e2e/jest.config.ts
@@ -0,0 +1,17 @@
+/* eslint-disable */
+export default {
+  displayName: 'server-app-e2e',
+  preset: '../../jest.preset.js',
+  setupFiles: ['<rootDir>/src/test-setup.ts'],
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': [
+      'ts-jest',
+      {
+        tsconfig: '<rootDir>/tsconfig.spec.json',
+      },
+    ],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/server-app-e2e',
+};
